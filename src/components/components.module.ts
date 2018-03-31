dc3f97c0b8aacc50438cd3e50bd9609b7eab036c
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-03-31 16:20:06 -0400

    saving before pulling

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { ResultCardComponent } from './result-card/result-card';
+@NgModule({
+	declarations: [ResultCardComponent],
+	imports: [],
+	exports: [ResultCardComponent]
+})
+export class ComponentsModule {}
